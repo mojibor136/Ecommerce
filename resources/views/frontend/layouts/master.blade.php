3b65043a34e3d93d2ddaf33b7aede89b2a60ff9e
MD MOJIB OR RAHMAN <mojibor6321@gmail.com>
2025-11-22 19:38:37 +0600

    update

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -10,6 +10,53 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
+    @if (!empty($pixelTracking))
+        <script>
+            ! function(f, b, e, v, n, t, s) {
+                if (f.fbq) return;
+                n = f.fbq = function() {
+                    n.callMethod ?
+                        n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+                };
+                if (!f._fbq) f._fbq = n;
+                n.push = n;
+                n.loaded = !0;
+                n.version = '2.0';
+                n.queue = [];
+                t = b.createElement(e);
+                t.async = !0;
+                t.src = v;
+                s = b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t, s)
+            }(window, document, 'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '{{ $pixelTracking }}');
+            fbq('track', 'PageView');
+        </script>
+        <noscript>
+            <img height="1" width="1" style="display:none"
+                src="https://www.facebook.com/tr?id={{ $pixelTracking }}&ev=PageView&noscript=1" />
+        </noscript>
+    @endif
+
+    @if (!empty($gtmTracking))
+        <script>
+            (function(w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({
+                    'gtm.start': new Date().getTime(),
+                    event: 'gtm.js'
+                });
+                var f = d.getElementsByTagName(s)[0],
+                    j = d.createElement(s),
+                    dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', '{{ $gtmTracking }}');
+        </script>
+    @endif
+
     @php
         $favicon = $setting->favicon;
         $logo = $setting->icon;
@@ -50,6 +97,13 @@
 @include('massage.index')
 
 <body class="bg-gray-100 text-gray-800">
+
+    @if (!empty($gtmTracking))
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id={{ $gtmTracking }}" height="0" width="0"
+                style="display:none;visibility:hidden"></iframe>
+        </noscript>
+    @endif
 
     <div class="bg-gray-800 w-full">
         <div class="max-w-6xl mx-auto flex items-center py-2">
